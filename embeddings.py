e647c08e9c064fbb1a51494484cdc4d7cdb40385
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-06-05 22:28:36 -0400

    Add function to create embeddings

--- embeddings.py
+++ embeddings.py
@@ -1,6 +1,30 @@
-from sentence_transformers import SentenceTransformer, util
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
 
-model = SentenceTransformer('all-mpnet-base-v2')
 
-def encode(sentences):
-    return model.encode(sentences)
+load_dotenv()
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+
+def create_embeddings(input):
+
+    # Ensure the input does not exceed the max content length
+    max_tokens = 8000
+    max_characters = max_tokens * 4
+
+    if isinstance(input, str):
+        input = input[:max_characters]
+    elif isinstance(input, list):
+        for i in range(len(input)):
+            input[i] = input[i][:max_characters]
+
+    # Create the embeddings
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=input
+    )
+    embeddings = [
+        item['embedding'] for item in response.to_dict()['data']
+    ]
+    return embeddings
