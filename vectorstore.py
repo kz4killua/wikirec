02ad5550125da487daabf602c4d4466a0cac5e57
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-06-05 23:03:34 -0400

    Batch vectorstore upserts

--- vectorstore.py
+++ vectorstore.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from pinecone import Pinecone
+import itertools
+from tqdm import tqdm
 
 
 load_dotenv()
@@ -8,9 +10,19 @@ pc = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
 index = pc.Index(os.getenv('PINECONE_INDEX_NAME'))
 
 
+
+def chunks(iterable, batch_size, desc=None):
+    """A helper function to break an iterable into chunks of size batch_size."""
+    it = iter(tqdm(iterable, desc=desc))
+    chunk = tuple(itertools.islice(it, batch_size))
+    while chunk:
+        yield chunk
+        chunk = tuple(itertools.islice(it, batch_size))
+
+
 def upsert_vectors(vectors, namespace):
     """
     https://docs.pinecone.io/reference/api/data-plane/upsert
     """
-    response = index.upsert(vectors, namespace=namespace)
-    return response
+    for vectors_chunk in chunks(vectors, 100, "Upserting vectors"):
+        index.upsert(vectors_chunk, namespace=namespace)
