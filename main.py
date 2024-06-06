f13c3744f7ad53cb0e5442f5239eb975c16c24df
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-06-05 22:35:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from wikirec import wikirec
-
-
 def main():
-
-    # Define likes and dislikes
-    likes = [
-        "Silicon Valley (TV series)",
-        "The Internship",
-        "The Social Network",
-        "Pirates of Silicon Valley",
-        "The IT Crowd",
-        "The Imitation Game",
-    ]
-    dislikes = [
-        "Drug discovery",
-    ]
-
-    recommendations = wikirec(likes, dislikes, ['articles'], 5, 5)
-
-    return recommendations
+    pass
 
 
 if __name__ == "__main__":
