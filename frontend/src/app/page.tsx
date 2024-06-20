9147773aa99d0c0d979e1468ebcde8e784ea38e4
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-06-19 21:53:21 -0400

    Include (perhaps preliminary) footer.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
         <Hero />
         <Features />
         <App />
+        <Footer />
       </main>
     </div>
   );
@@ -293,5 +294,31 @@ function UserPreference({
         </SelectContent>
       </Select>
     </div>
+  )
+}
+
+
+function Footer() {
+  return (
+    <section className="flex flex-col gap-y-6 py-12 px-12 bg-gray-300/50 border-t">
+      <div className="flex gap-2 items-center">
+        <Icon 
+          width={20}
+          height={20}
+        />
+        <div>
+          <strong className="font-extrabold tracking-tight text-base md:text-lg">
+            Wiki Rec
+          </strong>
+          <span className="ml-2 text-sm italic">
+            (Winner of the 2023 Code in my Bones hackathon!)
+          </span>
+        </div>
+      </div>
+      <div className="text-sm underline-offset-4">
+        Made by <Link href={"/"} className="font-medium underline underline-offset-4">Ifeanyi</Link>.
+        The source code is available on <Link href={"https://github.com/kz4killua/wikirec"} className="font-medium underline">Github</Link>.
+      </div>
+    </section>
   )
 }
