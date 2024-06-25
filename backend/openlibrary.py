f953feebb20d1fa51e1e83f7994c7ab9b2ad4ef0
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-06-24 22:20:19 -0400

    Backend: Add code to retrieve data from Open Library

---
+++ backend/openlibrary.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+ACCOUNT_EMAIL = os.getenv("OPENLIBRARY_ACCOUNT_EMAIL")
+
+headers = {
+    "User-Agent": f"WikiRec/1.0 ({ACCOUNT_EMAIL}@gmail.com)"
+}
+
+
+def search(q, limit, fields='*'):
+    # https://openlibrary.org/dev/docs/api/search
+    url = "https://openlibrary.org/search.json"
+    parameters = {'q': q, 'fields': fields, 'limit': 1}
+    response = requests.get(url, headers=headers, params=parameters)
+    return response.json()
+
+
+def covers(olid, size="L"):
+    # https://openlibrary.org/dev/docs/api/covers
+    url = f"https://covers.openlibrary.org/b/olid/{olid}-{size}.jpg"
+    return url
