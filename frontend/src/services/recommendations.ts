b04af563361a344f790814aab9a71971fb3c51b2
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-06-29 14:55:06 -0400

    Frontend: Update the API to get recommendations

--- frontend/src/services/recommendations.ts
+++ frontend/src/services/recommendations.ts
@@ -4,43 +4,12 @@ import { type Recommendation } from "@/types";
 
 
 export async function getRecommendations(wikipediaKeys: string[], category: string) {
-  // Temporarily return dummy data
-  const data: Recommendation[] = [
-    {
-      wikipedia_id: '1',
-      wikipedia_key: 'Harry_Potter',
-      wikipedia_title: 'Harry Potter',
-      title: 'Percy Jackson',
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/en/6/6b/Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg',
-    },
-    {
-      wikipedia_id: '2',
-      wikipedia_key: 'Harry_Potter',
-      wikipedia_title: 'Harry Potter',
-      title: 'Percy Jackson',
-      thumbnail: '',
-    },
-    {
-      wikipedia_id: '3',
-      wikipedia_key: 'Harry_Potter',
-      wikipedia_title: 'Harry Potter',
-      title: 'Percy Jackson',
-      thumbnail: '',
-    },
-    {
-      wikipedia_id: '4',
-      wikipedia_key: 'Harry_Potter',
-      wikipedia_title: 'Harry Potter',
-      title: 'Percy Jackson',
-      thumbnail: '',
-    },
-    {
-      wikipedia_id: '5',
-      wikipedia_key: 'Harry_Potter',
-      wikipedia_title: 'Harry Potter',
-      title: 'Percy Jackson',
-      thumbnail: '',
-    },
-  ]
-  return data
+  return await fetch("https://isgzjh3d3gm6tls2z5dmlpsewq0gkokq.lambda-url.us-east-1.on.aws/", {
+    method: "POST",
+    body: JSON.stringify({
+      page_keys: wikipediaKeys,
+      item_category: category
+    })
+  })
+  .then(res => res.json())
 }
