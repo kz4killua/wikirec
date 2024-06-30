0198da5a3a67ee8ca0e54626e853a857d8e7a906
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-06-29 21:17:44 -0400

    Backend: Add `bundle.sh` to create deployment package

---
+++ backend/bundle.sh
@@ -0,0 +1,15 @@
+mkdir package
+
+pip install \
+--platform manylinux2014_x86_64 \
+--target=package \
+--implementation cp \
+--python-version 3.12 \
+--only-binary=:all: --upgrade \
+-r requirements.txt
+
+cd package
+zip -r ../deployment_package.zip .
+
+cd ..
+zip deployment_package.zip *.py
