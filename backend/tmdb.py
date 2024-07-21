751e4f0d128b74a98ef72e7781b0a44fca66c264
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-07-21 03:07:53 -0400

    Backend: Add functions for retrieving popular TV series and movies

--- backend/tmdb.py
+++ backend/tmdb.py
@@ -41,3 +41,33 @@ def get_image_url(file_path, file_size="w500"):
     # https://developer.themoviedb.org/docs/image-basics
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{file_size}{file_path}"
+
+
+def list_popular_tv_series(language="en-US", page=1):
+    # https://developer.themoviedb.org/reference/tv-series-popular-list
+    url = "https://api.themoviedb.org/3/tv/popular"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {ACCESS_TOKEN}"
+    }
+    params = {
+        "language": language,
+        "page": page
+    }
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
+
+
+def list_popular_movies(language="en-US", page=1):
+    # https://developer.themoviedb.org/reference/movie-popular-list
+    url = "https://api.themoviedb.org/3/movie/popular"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {ACCESS_TOKEN}"
+    }
+    params = {
+        "language": language,
+        "page": page
+    }
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
