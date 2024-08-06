e53f07a6615d540f54a9ae707e341dbf8e0f5c84
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-08-06 03:25:31 -0400

    Update font loading in layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Wiki Rec - Recommendations for anything!",
